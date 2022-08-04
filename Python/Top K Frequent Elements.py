9e12068fb1ed6430e096652c272b311a22d870bc
DucAnh <nguyenducanhvg239@gmail.com>
2022-08-04 17:54:32 +0700

    4/8 code

---
+++ Python/Top K Frequent Elements.py
@@ -0,0 +1,13 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashlist = {} # create hashmap using dict
+        for i in nums:
+            if i not in hashlist: 
+                hashlist[i] = 1 # add i to hashmap
+            else:
+                hashlist[i] = hashlist[i] + 1
+        # sort dict by value
+        hashlist = dict(sorted(hashlist.items(), key=lambda item: item[1], reverse=True))
+        
+        # return a list of the first k key()
+        return list(hashlist.keys())[0:k]
